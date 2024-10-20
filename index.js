50122e97c8ade3cfac30879ab556b6294464a2b9
Varun Rewadi <varunrewadi.vr005@gmail.com>
2024-10-20 22:14:10 +0530

    added app.set for views

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const path = require('path');
 const axios = require('axios');
 const dotenv = require('dotenv');
 
-if (process.env.NODE_ENV !== 'production') {
-    dotenv.config();
-  }
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json()); // To parse incoming JSON requests
 
